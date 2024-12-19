e8bec118dea29147c93015940ce6f5ae937c66a2
Daryl Fong <darkare@gmail.com>
2024-12-19 16:38:08 +1100

    add video prd

---
+++ src/lib/video/VideoHeader.svelte
@@ -0,0 +1,26 @@
+<style>
+	.header {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		padding: 10px;
+		background-color: white;
+		font-weight: bold;
+	}
+
+	.header-item {
+		padding: 0 10px;
+		color: black;
+	}
+
+	.header-item.active {
+		color: black;
+	}
+</style>
+
+<div class="header">
+	<div class="header-item active">ALL VIDEOS</div>
+	<div class="header-item">BEAUTY</div>
+	<div class="header-item">COMEDY</div>
+	<div class="header-item">VERY PERI</div>
+</div>
