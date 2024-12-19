a8e95f8e889ea15af087759732c807f68943ae4f
Daryl Fong <darkare@gmail.com>
2024-12-19 16:44:51 +1100

    initial video header component

---
+++ src/lib/video/VideoHeader.svelte
@@ -0,0 +1,32 @@
+<script>
+	import VideoStore from '$lib/components/video/VideoStore.svelte.js';
+
+	let topics = VideoStore.getTopics();
+</script>
+
+<style>
+	.header {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		padding: 10px;
+		background-color: white;
+		font-weight: bold;
+	}
+
+	.header-item {
+		padding: 0 10px;
+		color: black;
+	}
+
+	.header-item.active {
+		color: black;
+	}
+</style>
+
+<div class="header">
+	<div class="header-item active">ALL VIDEOS</div>
+	{#each topics as topic}
+		<div class="header-item">{topic}</div>
+	{/each}
+</div>
